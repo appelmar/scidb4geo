800ac0af6f1b73e3f62e30ec81d86d3895454c1f
Marius Appel <marius.appel@uni-muenster.de>
2016-04-01 21:29:36 +0200

    Update automatic documentation build

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+# scidb4geo - A SciDB plugin for managing spatially referenced arrays
+# Copyright (C) 2016 Marius Appel <marius.appel@uni-muenster.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+
 TESTARRAY=scidb4geo_test_$( date +%Y%m%d_%H%M%S )
 iquery -anq "remove(${TESTARRAY});" > /dev/null 2>&1
 iquery -anq "store(build(<val:double>[lat=0:179,256,0,lon=0:359,256,0, t=0:31,16,0],1),${TESTARRAY});" > /dev/null 2>&1
--- tools/make_doc.sh
+++ tools/make_doc.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
-# This file sets up the platform for automated documentation builds with travis ci.
 
-# 1. install dependencies (pandoc)
-wget https://github.com/jgm/pandoc/releases/download/1.17.0.2/pandoc-1.17.0.2-1-amd64.deb
-yes | sudo gdebi pandoc-1.17.0.2-1-amd64.deb
+# scidb4geo - A SciDB plugin for managing spatially referenced arrays
+# Copyright (C) 2016 Marius Appel <marius.appel@uni-muenster.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
 
-# 2. Make documentation 
+
+# This file makes HTML documentation files and pushes changes to gh-pages branch
+
+
+# 1. clean up
 rm -rf doc/pub
 mkdir -p doc/pub
 mkdir -p doc/pub/operators
 
-
+# 2. call pandoc for all Markdown files
 MDFILES=$( find doc/operators -iname "*.md" -type f )
 for f in $MDFILES
 do
   pandoc $f --to html --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures  --output ${f/.md/.html} --smart --mathjax --email-obfuscation none --self-contained --standalone --section-divs
 done
   
-# copy public doc to gh-pages folder
+# 3. copy public doc to local gh-pages folder
 cp doc/operators/*.html doc/pub/operators/
- 
 cd ../doc/pub
+
+# 4. make this folder as the root of gh-pages branch
 git init
 git config user.name "Travis CI Documentation Builder"
 git config user.email "marius.appel@uni-muenster.de"
